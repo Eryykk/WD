bf7b9df6ecdd71a33019b9a560bc69e8a7c4bb6b
Eryk <eryk2420000@o2.pl>
2021-03-12 17:26:13 +0100

    zadania

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-import sys as system
+#lab 1
+#zad 1
+a = "ala ma kota"
+b = "ala ma psa"
+print(a)
+print(b)
 
-print(system.version)
-fdfddf
+x = 1
+y = 2
+print(x)
+print(y)
+
+d = 2.1
+t = 3.2
+print(d)
+print(t)
+
+p = 3+5j
+d = 5j
+print(p)
+print(d)
+
+
+#zad 2
