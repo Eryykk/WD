8515742c8fe58d048917b623da2fbc2158c54565
Eryk <eryk2420000@o2.pl>
2021-03-18 21:21:32 +0100

    jj

--- lab3.py
+++ lab3.py
@@ -1,2 +1,32 @@
-p="heluhjjhgjlo"
-print (p)
+#lab 1
+#zad 1
+a = "ala ma kota"
+b = "ala ma psa"
+print(a)
+print(b)
+
+x = 1
+y = 2
+print(x)
+print(y)
+
+d = 2.1
+t = 3.2
+print(d)
+print(t)
+
+p = 3+5j
+d = 5j
+print(p)
+print(d)
+
+
+#zad 2
+suma = 2 + 2 * 2 / 2.0
+print(suma)
+
+kwadrat = 7 ** 2
+print(kwadrat)
+
+odejmowanie = 2 - 1
+print(odejmowanie)
