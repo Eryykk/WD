d7aaf3a737e2194cf6d15bbe8e3dcbac9d35285c
Eryk <eryk2420000@o2.pl>
2021-03-18 21:37:37 +0100

    zadania

--- lab2.py
+++ lab2.py
@@ -2,3 +2,17 @@
 datainput = input(u"Wpisz dowolny tekst zawierający litere 'a' : ")
 datacount = datainput.count("a") + datainput.count("A")
 print(u"Litery 'a' uzyles:", datacount, "razy.")
+
+#zadanie2
+import sys as system
+system.stdout.write(u"Wpisz liczbę całkowitą 'a': ")
+rla = system.stdin.readline()
+
+system.stdout.write(u"Wpisz liczbę całkowitą 'b': ")
+rlb = system.stdin.readline()
+
+system.stdout.write(u"Wpisz liczbę całkowitą 'c': ")
+rlc = system.stdin.readline()
+
+result = (int(rla) ** int(rlb) + int(rlc))
+print(u"Wynik: ", result)
